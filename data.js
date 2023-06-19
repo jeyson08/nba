b001012a183677e69a401b04b92c70a263d20fa8
Jeyson Boursault <jeyson.boursault@gmail.com>
2023-06-19 10:01:35 +0200

    data

---
+++ data.js
@@ -0,0 +1,34 @@
+const data = [
+    {
+      "nom": "LeBron James",
+      "equipe": "Los Angeles Lakers",
+      "numero": 23,
+      "position": "Ailier"
+    },
+    {
+      "nom": "Stephen Curry",
+      "equipe": "Golden State Warriors",
+      "numero": 30,
+      "position": "Meneur"
+    },
+    {
+      "nom": "Kevin Durant",
+      "equipe": "Brooklyn Nets",
+      "numero": 7,
+      "position": "Ailier"
+    },
+    {
+      "nom": "Giannis Antetokounmpo",
+      "equipe": "Milwaukee Bucks",
+      "numero": 34,
+      "position": "Ailier fort"
+    },
+    {
+      "nom": "Kawhi Leonard",
+      "equipe": "Los Angeles Clippers",
+      "numero": 2,
+      "position": "Ailier"
+    }
+  ];
+  
+module.export = data;
